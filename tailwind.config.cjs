4ab4008139a5bf2f6e24e6a7a3eb9d375e00ec5b
unknown <akskarim17@gmail.com>
2023-02-07 04:23:43 +0600

    tailwindcss configured

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    'index.html',
+    './src/components/**/*.jsx',
+    './src/hooks/**/.js',
+    './src/pages/**/*.jsx',
+  ],
   theme: {
     extend: {},
   },
